c8f07a23fe40fd6ca32918aa99d0fd8a7fb9bd19
Victor Gheiler <victorgheiler3@icloud.com>
2024-05-06 23:38:34 -0400

    fix api call

--- index.ts
+++ index.ts
@@ -3,10 +3,8 @@ import cors from "cors";
 import { Queries } from "./types";
 import { generateUserPrompt } from "./query";
 import dotenv from "dotenv";
-import OpenAI from "openai";
 
 dotenv.config();
-console.log(process.env);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,30 +16,33 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-type GptResponse = {
-  answer: string;
-};
-
-const openai = new OpenAI({ apiKey: process.env.API_KEY });
-
 app.post("/makeapicall", async (req, res) => {
   const { queries }: { queries: Queries } = req.body;
 
   const userPrompt = generateUserPrompt(queries);
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    max_tokens: 500,
-    temperature: 0.5,
-    messages: [
-      {
-        role: "user",
-        content: userPrompt,
-      },
-    ],
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      max_tokens: 500,
+      temperature: 0.5,
+      messages: [
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+    }),
   });
 
-  res.send(response.choices[0].message.content);
+  const json: any = await response.json();
+
+  res.send(json.choices[0].messages.content);
 });
 
 app.listen(port, () => {
